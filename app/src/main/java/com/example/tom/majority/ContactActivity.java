75161d7c488683a83cb73854f70101fba2bfe688
Andy J Phillips <andrew.j.phillips@hotmail.co.uk>
2017-06-25 15:17:27 +0100

    Added handler/connection from MainActivity to LoginPage.

--- app/src/main/java/com/example/tom/majority/ContactActivity.java
+++ app/src/main/java/com/example/tom/majority/ContactActivity.java
@@ -17,10 +17,6 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
--- app/src/main/java/com/example/tom/majority/Userprofile.java
+++ app/src/main/java/com/example/tom/majority/Userprofile.java
@@ -36,10 +36,6 @@ public class Userprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
