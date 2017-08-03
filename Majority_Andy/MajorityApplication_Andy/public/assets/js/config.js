b4512a5dfdc950c08c24358ef477aaf853817420
Andy J Phillips <andrew.j.phillips@hotmail.co.uk>
2017-08-03 10:09:51 +0100

    Added new file structure to git: fresh commit.

---
+++ Majority_Andy/MajorityApplication_Andy/public/assets/js/config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDkoCXYDkvU0abL5nrx-GnIf6jpfKBxlyE",
+    authDomain: "majorityweb.firebaseapp.com",
+    databaseURL: "https://majorityweb.firebaseio.com",
+    projectId: "majorityweb",
+    storageBucket: "majorityweb.appspot.com",
+    messagingSenderId: "1052849591343"
+  };
+  "accountChooserEnabled",!0;
+  firebase.initializeApp(config);
