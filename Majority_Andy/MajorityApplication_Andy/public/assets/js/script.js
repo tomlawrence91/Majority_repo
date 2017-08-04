2e29dbf2620a04b3435674bf3515320063b89f56
Andy J Phillips <andrew.j.phillips@hotmail.co.uk>
2017-08-04 15:29:32 +0100

    Implemented create project. Users are now able to create projects through the create project page.

--- Majority_Andy/MajorityApplication_Andy/public/assets/js/script.js
+++ Majority_Andy/MajorityApplication_Andy/public/assets/js/script.js
@@ -1,79 +1,107 @@
+//---------------------------------------
+//Firebase interaction
+//---------------------------------------
 $(function () {
-    //Initialise firebase
-    var config = {
-      apiKey: "AIzaSyDkoCXYDkvU0abL5nrx-GnIf6jpfKBxlyE",
-      authDomain: "majorityweb.firebaseapp.com",
-      databaseURL: "https://majorityweb.firebaseio.com",
-      projectId: "majorityweb",
-      storageBucket: "majorityweb.appspot.com",
-      messagingSenderId: "1052849591343"
+    const config = {
+        //Initialise Firebase
+        apiKey: "AIzaSyDkoCXYDkvU0abL5nrx-GnIf6jpfKBxlyE",
+        authDomain: "majorityweb.firebaseapp.com",
+        databaseURL: "https://majorityweb.firebaseio.com",
+        projectId: "majorityweb",
+        storageBucket: "majorityweb.appspot.com",
+        messagingSenderId: "1052849591343"
     };
     firebase.initializeApp(config);
-    //database ref
-    var database = firebase.database();
-    //Login user
+    const database = firebase.database(); //Reference Database
+
+//---------------------------------------
+//User creation, login, logout
+//---------------------------------------
+
+//User Login
     $('.login-form').on('submit', event => {
         event.preventDefault();
         const email = $('#login-email').val();
         const password = $('#login-password').val();
         console.log(email, password);
         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-
         });
         firebase.auth().onAuthStateChanged(function(user) {
-              if (user) {
+            if (user) {
                 window.location.href = "home.html";
-              } else {
-
+            } else {
               }
         })
     })
 
-    //Create user
+
+//User Creation
     $('.register-form').on('submit', event => {
         event.preventDefault();
         const email = $('#reg-email').val();
         const password = $('#reg-password').val();
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // ...
+            var errorCode = error.code;
+            var errorMessage = error.message;
         });
         firebase.auth().onAuthStateChanged(function(user) {
-              if (user) {
-                window.location.href = "home.html";
-              } else {
-
-              }
+            if (user) {
+            window.location.href = "home.html";
+            } else {
+            }
         })
     })
 
-})
 
-//Logout user
-$( ".logout" ).click(function(e) {
-    e.preventDefault();
-    firebase.auth().signOut().then(function() {
-        alert("user out");
-      window.location.href = "index.html";
-    }).catch(function(error) {
-      // An error happened.
-    });
+//User Logout
+    $('.logout').on('click', event => {
+        event.preventDefault();
+        firebase.auth().signOut().then(function() {
+            window.location.href = "index.html";
+        }).catch(function(error) {
+          // An error happened.
+        });
+    })
+
+
+//---------------------------------------
+//Project creation, view, edit etc.
+//---------------------------------------
+
+//Add Project
+    $('#submit-project').on('click', event => {
+        event.preventDefault();
+        const projectName = $('#project-name').val();
+        const projectSummary = $('#project-summary').val();
+        const projectDetail = $('#project-detail').val();
+        const budgetEstimate = $('#budget-estimate').val();
+        const dateAdded = Date.now();
+        const image = "image";
+        const dbRef = firebase.database().ref();
+        var storesRef = dbRef.child('/project/');
+        var newStoreRef = storesRef.push();
+        newStoreRef.set({
+            "projectName": projectName,
+            "projectSummary": projectSummary,
+            "projectDetail": projectDetail,
+            "budgetEstimate": budgetEstimate,
+            "dateAdded": dateAdded,
+            "image": image,
+        });
+        console.log(projectName, projectSummary, projectDetail, budgetEstimate, dateAdded );
+    })
+
 });
 
-
-
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-
-    function htmlbodyHeightUpdate() {
+//---------------------------
+// Controller scripts
+//---------------------------
+function htmlbodyHeightUpdate() {
     var height3 = $(window).height();
     var height1 = $('.nav').height() + 50;
-    height2 = $('.container-main').height();
+    var height2 = $('.container-main').height();
     if (height2 > height3) {
         $('html').height(Math.max(height1, height3, height2) + 10);
         $('body').height(Math.max(height1, height3, height2) + 10);
@@ -84,7 +112,9 @@ $( ".logout" ).click(function(e) {
     }
 
 }
+//document.ready
 $(document).ready(function () {
+
     htmlbodyHeightUpdate();
     $(window).resize(function () {
         htmlbodyHeightUpdate();
@@ -94,9 +124,9 @@ $(document).ready(function () {
         htmlbodyHeightUpdate();
     });
 
-
+    //toggles user login and create user
     $('.message a').click(function(){
-       $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
     });
 
 });
